c7c26260271ac4a3d49cc8d8f2de93d1c4ea0fd5
vinoth <vinoth.alagendran@photoninfotech.net>
2015-11-17 15:15:17 +0530

    Removing old files

--- admin/db_scripts/course_sections.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `mdl_course_sections` ADD `timemodified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `setlogo`;
-UPDATE `mdl_course_sections` SET `timemodified`=NOW();
--- admin/local/newuser/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2014082801; // Plugin version.
-$plugin->requires = 2012120300; // Moodle version.
-$plugin->component = 'local_newuser'; // Full name of the plugin (used for diagnostics).
-
