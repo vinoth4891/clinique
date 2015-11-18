a1c36b50909e3334ba3962c6230b4ca894e357bc
srinivasan_t <srinivasan.tharmar@photoninfotech.net>
2015-11-18 21:06:05 +0530

    need to test resize issue.

--- components/match/js/Match.js
+++ components/match/js/Match.js
@@ -824,7 +824,7 @@ define(["framework/WidgetWithTemplate","courseItem/CourseItem","course/Course","
 			
 			/* Start's Text Alignment based on number of lines */
 			
-			$( window ).resize(function() {
+			/* $( window ).resize(function() {
 				if(!$('html').hasClass('ie8') && !$('html').hasClass('ie9')){
 					var divheight = $("#quesname_activity2").height(); 
 					var lineheight = $("#quesname_activity2").css('line-height').replace("px","");
@@ -835,7 +835,7 @@ define(["framework/WidgetWithTemplate","courseItem/CourseItem","course/Course","
 						$("#quesname_activity2").addClass("righttalign");     
 					}				
 				}
-			});
+			}); */
 			/* End of Text Alignment based on number of lines */
         }
     });
