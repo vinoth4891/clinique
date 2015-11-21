8f03a5a54d4a046b86eebfbe173a8293ca1b19b2
srinivasan_t <srinivasan.tharmar@photoninfotech.net>
2015-11-21 15:10:33 +0530

    "replace" script error issue fixed.

--- assets/www/components/match/js/Match.js
+++ assets/www/components/match/js/Match.js
@@ -828,7 +828,7 @@ define(["framework/WidgetWithTemplate","courseItem/CourseItem","course/Course","
 			/* Start's Text Alignment based on number of lines */
 			
 			$( window ).resize(function() {
-				if(!$('html').hasClass('ie8') && !$('html').hasClass('ie9')){
+				if(!$('html').hasClass('ie8') && !$('html').hasClass('ie9') && $("#quesname_activity2").length > 0){
 					var divheight = $("#quesname_activity2").height(); 
 					var lineheight = $("#quesname_activity2").css('line-height').replace("px","");
 					var count = Math.round(divheight/parseInt(lineheight));
